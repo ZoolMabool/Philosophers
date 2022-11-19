7b6506b35a6f46454188a3a722daef2c23ecc0b8
Zoolmabool <yan.baudouin@gmail.com>
2022-11-19 10:55:29 +0100

    push header

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: ybaudoui <ybaudoui@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 11:17:23 by ybaudoui          #+#    #+#             */
-/*   Updated: 2022/11/17 17:15:46 by ybaudoui         ###   ########.fr       */
+/*   Updated: 2022/11/18 17:03:47 by ybaudoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,10 @@ typedef struct s_mutexes
 	pthread_mutex_t	print_lock;
 	pthread_mutex_t	*fork;
 	pthread_mutex_t	philo_die;
+	pthread_mutex_t	eat_enough;
+	pthread_mutex_t	all_eat;
 	pthread_mutex_t	philo_eat;
+	pthread_mutex_t	number_of_meal;
 }	t_mutexes;
 
 typedef struct s_philo_data
@@ -41,6 +44,8 @@ typedef struct s_philo_data
 	int				philo_die;
 	long int		start_time;
 	int				philo_number;
+	int				eat_enough;
+	int				max_eat;
 	t_mutexes		*mutexes;
 }	t_philo_data;
 
@@ -50,29 +55,54 @@ typedef struct s_data
 	long int			time_to_die;
 	long int			time_to_eat;
 	long int			time_to_sleep;
+	int					all_eat;
 	long int			number_philo_must_eat;
 	long int			time_start;
 	pthread_t			*philo;
 	pthread_t			death;
+	pthread_t			fat;
 	t_philo_data		*philo_data;
 	t_mutexes			*mutexes;
 }	t_data;
 
-int			ft_atoi(const char *nptr);
+/*****liB*****/
+
+int			ft_atoi(const char *nptr, int *error);
 int			ft_isdigit(int character);
+
+/*****error*****/
+
 void		print_error(void);
-int			check_num(char **argv);
-int			verif_arg(char **argv);
+void		print_error_digit(void);
+
+/*****init*****/
+
+int			init_struct(t_data *data, char **argv, t_mutexes *mutexes);
+int			init_philo(t_data *data, t_mutexes *mutexes);
+void		init_death(t_data *data);
+int			init_mutex(t_mutexes *mutexes, int number_philo);
+
+/*****timestamp*****/
+
 long int	get_time(void);
 long int	current_time(long int time_start);
-void		*routine(void *data);
-void		print_messages(t_philo_data *philo_data, char *message, int flag);
-int			struct_init(t_data *data, char **argv, t_mutexes *mutexes);
-int			philo_init(t_data *data, t_mutexes *mutexes);
-int			init_mutex(t_mutexes *mutexes, int number_philo);
-void		*check_death(void *arg);
-void		init_death(t_data *data);
+
+/*****parsing*****/
+
+int			verif_arg(int argc, char **argv);
+
+/*****clean*****/
+
 void		clean_program(t_mutexes *mutexes, t_data *data);
 void		ft_free(t_data *data, t_mutexes *mutexes);
+
+/*****print*****/
+
+void		print_messages(t_philo_data *philo_data, char *message, int flag);
+
+/*****program*****/
+void		*check_death(void *arg);
+void		*routine(void *data);
+void		*check_meal(void *arg);
 
 #endif
